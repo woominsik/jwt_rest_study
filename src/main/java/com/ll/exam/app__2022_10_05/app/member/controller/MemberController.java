37e918bb5733954cda61487376747566929fc2ce
woominsik <9737638@naver.com>
2022-10-07 22:16:33 +0900

    ResponseEntity로 authentication헤더 추가

--- src/main/java/com/ll/exam/app__2022_10_05/app/member/controller/MemberController.java
+++ src/main/java/com/ll/exam/app__2022_10_05/app/member/controller/MemberController.java
@@ -1,17 +1,28 @@
 package com.ll.exam.app__2022_10_05.app.member.controller;
 
 import lombok.Data;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.net.http.HttpResponse;
+
 @RestController
 @RequestMapping("/member")
 public class MemberController {
     @PostMapping("/login")
-    public String login(@RequestBody LoginDto loginDto) {
-        return "username : %s, password : %s".formatted(loginDto.getUsername(), loginDto.getPassword());
+    public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authentication", "JWT키");
+
+        String body = "username : %s, password : %s".formatted(loginDto.getUsername(), loginDto.getPassword());
+
+        return new ResponseEntity<>(body, headers, HttpStatus.OK);
     }
 
     @Data
