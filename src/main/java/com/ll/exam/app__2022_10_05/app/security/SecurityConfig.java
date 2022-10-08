572cdbdc60a124bebcbb99dcdce3b43d36be56e5
woominsik <9737638@naver.com>
2022-10-08 23:23:01 +0900

    로그인과 회원가입 엔드 포인트 제외하고, 전무 인증필요, 필터추가

--- src/main/java/com/ll/exam/app__2022_10_05/app/security/SecurityConfig.java
+++ src/main/java/com/ll/exam/app__2022_10_05/app/security/SecurityConfig.java
@@ -1,28 +1,40 @@
 package com.ll.exam.app__2022_10_05.app.security;
 
+import com.ll.exam.app__2022_10_05.app.security.filter.JwtAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtAuthorizationFilter jwtAuthorizationFilter;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationConfiguration authenticationConfiguration) throws Exception {
         http
+                .authorizeRequests(
+                        authorizeRequests -> authorizeRequests
+                                .antMatchers("/member/login", "/member/join")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated() // 최소자격 : 로그인
+                )
                 .cors().disable() // 타 도메인에서 API 호출 가능
                 .csrf().disable() // CSRF 토큰 끄기
                 .httpBasic().disable() // httpBaic 로그인 방식 끄기
                 .formLogin().disable() // 폼 로그인 방식 끄기
                 .sessionManagement(sessionManagement ->
                         sessionManagement.sessionCreationPolicy(STATELESS)
-                ); // 세션 사용안함
-
+                ).addFilterBefore(
+                        jwtAuthorizationFilter,
+                        UsernamePasswordAuthenticationFilter.class
+                );
         return http.build();
     }
 }
