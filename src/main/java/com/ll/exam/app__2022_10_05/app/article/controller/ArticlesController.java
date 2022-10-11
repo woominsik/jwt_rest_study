39af8c9c154161b62969c4a5d485b7cd994184f3
woominsik <9737638@naver.com>
2022-10-11 10:09:43 +0900

    GET /article/1

--- src/main/java/com/ll/exam/app__2022_10_05/app/article/controller/ArticlesController.java
+++ src/main/java/com/ll/exam/app__2022_10_05/app/article/controller/ArticlesController.java
@@ -7,6 +7,7 @@ import com.ll.exam.app__2022_10_05.util.Util;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +28,28 @@ public class ArticlesController {
                 RsData.successOf(
                         Util.mapOf(
                                 "articles", articles
+                        )
+                )
+        );
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<RsData> detail(@PathVariable Long id) {
+        Article article = articleService.findById(id).orElse(null);
+
+        if (article == null) {
+            return Util.spring.responseEntityOf(
+                    RsData.of(
+                            "F-1",
+                            "해당 게시물은 존재하지 않습니다."
+                    )
+            );
+        }
+
+        return Util.spring.responseEntityOf(
+                RsData.successOf(
+                        Util.mapOf(
+                                "article", article
                         )
                 )
         );
