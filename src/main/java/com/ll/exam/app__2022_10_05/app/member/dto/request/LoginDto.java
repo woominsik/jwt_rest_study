0e3fa6eb93ff3d0661fa5fc3be20a0ee16f409b8
woominsik <9737638@naver.com>
2022-10-11 10:30:38 +0900

    입력 데이터 유효성 체크 자동화

--- src/main/java/com/ll/exam/app__2022_10_05/app/member/dto/request/LoginDto.java
+++ src/main/java/com/ll/exam/app__2022_10_05/app/member/dto/request/LoginDto.java
@@ -2,9 +2,13 @@ package com.ll.exam.app__2022_10_05.app.member.dto.request;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 public class LoginDto {
+    @NotEmpty(message = "username 을(를) 입력해주세요.")
     private String username;
+    @NotEmpty(message = "password 을(를) 입력해주세요.")
     private String password;
 
     public boolean isNotValid() {
