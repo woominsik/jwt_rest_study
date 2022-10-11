76c5daa3b502fc2cd74424a6c0d95d4eb3978877
woominsik <9737638@naver.com>
2022-10-11 10:18:31 +0900

    PATCH /articles/1

--- src/main/java/com/ll/exam/app__2022_10_05/app/article/controller/ArticlesController.java
+++ src/main/java/com/ll/exam/app__2022_10_05/app/article/controller/ArticlesController.java
@@ -1,5 +1,6 @@
 package com.ll.exam.app__2022_10_05.app.article.controller;
 
+import com.ll.exam.app__2022_10_05.app.article.dto.request.ArticleModifyDto;
 import com.ll.exam.app__2022_10_05.app.article.entity.Article;
 import com.ll.exam.app__2022_10_05.app.article.service.ArticleService;
 import com.ll.exam.app__2022_10_05.app.base.dto.RsData;
@@ -10,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Scanner;
 
@@ -82,6 +84,38 @@ public class ArticlesController {
                 RsData.of(
                         "S-1",
                         "해당 게시물이 삭제되었습니다."
+                )
+        );
+    }
+
+    @PatchMapping("{id}")
+    public ResponseEntity<RsData> modify(@PathVariable Long id, @AuthenticationPrincipal MemberContext memberContext, @Valid @RequestBody ArticleModifyDto articleModifyDto) {
+        Article article = articleService.findById(id).orElse(null);
+
+        if (article == null) {
+            return Util.spring.responseEntityOf(
+                    RsData.of(
+                            "F-1",
+                            "해당 게시물은 존재하지 않습니다."
+                    )
+            );
+        }
+
+        if (articleService.actorCanModify(memberContext, article) == false) {
+            return Util.spring.responseEntityOf(
+                    RsData.of(
+                            "F-2",
+                            "수정 권한이 없습니다."
+                    )
+            );
+        }
+
+        articleService.modify(article, articleModifyDto);
+
+        return Util.spring.responseEntityOf(
+                RsData.of(
+                        "S-1",
+                        "해당 게시물이 수정되었습니다."
                 )
         );
     }
--- src/main/java/com/ll/exam/app__2022_10_05/app/article/service/ArticleService.java
+++ src/main/java/com/ll/exam/app__2022_10_05/app/article/service/ArticleService.java
@@ -1,5 +1,6 @@
 package com.ll.exam.app__2022_10_05.app.article.service;
 
+import com.ll.exam.app__2022_10_05.app.article.dto.request.ArticleModifyDto;
 import com.ll.exam.app__2022_10_05.app.article.entity.Article;
 import com.ll.exam.app__2022_10_05.app.article.repository.ArticleRepository;
 import com.ll.exam.app__2022_10_05.app.member.entity.Member;
@@ -41,5 +42,15 @@ public class ArticleService {
 
     public boolean actorCanDelete(MemberContext memberContext, Article article) {
         return memberContext.getId() == article.getAuthor().getId();
+    }
+
+    public void modify(Article article, ArticleModifyDto articleModifyDto) {
+        article.setSubject(articleModifyDto.getSubject());
+        article.setContent(articleModifyDto.getContent());
+        articleRepository.save(article);
+    }
+
+    public boolean actorCanModify(MemberContext memberContext, Article article) {
+        return actorCanDelete(memberContext, article);
     }
 }
